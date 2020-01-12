cf3d397f6c1c584357cb296a123f5dd9fa695c2e
HimanshuRattan <35609063+HimanshuRattan@users.noreply.github.com>
2020-01-12 13:07:57 +0530

    user reviews and compessed images

---
+++ app.js
@@ -0,0 +1,26 @@
+$('.main-content .owl-carousel').owlCarousel({
+	stagePadding: 50,
+	loop: true,
+	margin: 10,
+	nav: true,
+	autoplay: true,
+	autoplayTimeout: 3000,
+	autoplayHoverPause: true,
+	dots: false,
+	navText: [
+		'<i class="fa fa-angle-left" aria-hidden="true"></i>',
+		'<i class="fa fa-angle-right" aria-hidden="true"></i>'
+	],
+	navContainer: '.main-content .custom-nav',
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 3
+		},
+		1000: {
+			items: 3
+		}
+	}
+});
